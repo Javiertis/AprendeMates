6a456a6ea8a59f293bf1831724aba96d15a000f5
Javier Tejdor <javi.teje99@gmail.com>
2020-11-12 10:12:16 +0100

    pasar nombre de principal a Dificultad

--- app/src/main/java/com/jtc/aprendemates/Dificultad.java
+++ app/src/main/java/com/jtc/aprendemates/Dificultad.java
@@ -6,9 +6,11 @@ import android.os.Bundle;
 
 public class Dificultad extends AppCompatActivity {
 
+    String nombre;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dificultad);
+        nombre = savedInstanceState.getString("Nombre", "Tú");
     }
 }
--- app/src/main/java/com/jtc/aprendemates/MainActivity.java
+++ app/src/main/java/com/jtc/aprendemates/MainActivity.java
@@ -2,13 +2,28 @@ package com.jtc.aprendemates;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText nombre;
+    Button boton_empezar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        nombre=findViewById(R.id.txtNombre);
+        boton_empezar=findViewById(R.id.btEmpezar);
+        boton_empezar.setOnClickListener(v -> onClickBotonEmpezar());
+    }
+    void onClickBotonEmpezar(){
+        Intent i =new Intent(this, Dificultad.class);
+        Bundle b=new Bundle();
+        b.putString("nombre", nombre.getText().toString());
+        startActivity(i,b);
     }
 }
