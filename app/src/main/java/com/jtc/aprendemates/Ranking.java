74ecc24f64093a7223110bf9ff5d05d3daf7088e
Javiertis <javi.teje99@gmail.com>
2020-11-23 13:09:10 +0100

    formatting a string

--- app/src/main/java/com/jtc/aprendemates/Ranking.java
+++ app/src/main/java/com/jtc/aprendemates/Ranking.java
@@ -33,10 +33,10 @@ public class Ranking extends AppCompatActivity {
         while (row.moveToNext()) {
             txt = new TextView(this);
             String str = String.format(Locale.ENGLISH, "%s %td/%2$tm/%2$ty", row.getInt(5), row.getLong(0));
-            str = String.format(Locale.ENGLISH, "%02d.- %-2.4s%-15.14s%s", i, row.getInt(4), row.getString(1), str);
+            str = String.format(Locale.ENGLISH, "%02d.- %-4.4s%-15.14s%s", i, row.getInt(4), row.getString(1), str);
             txt.setText(str);
             txt.setTextAppearance(R.style.txtGame);
-            txt.setTextSize(20f);
+            txt.setTextSize(17f);
             txt.setTypeface(Typeface.MONOSPACE);
             rank.addView(txt);
             i++;
